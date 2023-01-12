c8a20d1465f5b10de0763bdf28651bb71c45b284
vishvajeet1999 <vishvajeet141@gmail.com>
2023-01-12 16:51:26 +0530

    recommitted

---
+++ connection.php
@@ -0,0 +1,12 @@
+
+<?php 
+
+$link = mysqli_connect("localhost","fighsfghfdh","#fhfhsfgshdgd","gdfhfhfgf");
+if(mysqli_connect_error()){
+    die("ERROR: Unable to connect:". mysqli_connect_error());
+    echo"<script>window.alert('Hi')</script>"; 
+    
+ }    
+  
+?>
+ 
